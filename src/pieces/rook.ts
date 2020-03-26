e8996a1e90c1083b007866f7bc80d20ce3389013
matheusmosca <matheusmoscag@gmail.com>
2020-03-26 15:49:52 -0300

    Rook movement

--- src/pieces/rook.ts
+++ src/pieces/rook.ts
@@ -1,9 +1,69 @@
 import { Piece, IPiece, Color } from './piece';
+import { ICoordinate } from '../game';
 
 export class Rook extends Piece implements IPiece {
     
     constructor(row: number, column: number, color: Color) {
         super(row, column, color);
         this.Moved = false;
+    }
+
+    possibleMovementsList(board: Piece[][]): ICoordinate[] {
+        const listOfCoordinates: ICoordinate[] = [];
+        let i = this.row;
+        let j = this.column + 1;
+
+        for (j; j < 8; j++) {
+            if (board[i][j] === null) {
+                listOfCoordinates.push({ row: i, column: j });
+            } else if (board[i][j].color !== this.color) {
+                listOfCoordinates.push({ row: i, column: j });
+                break;
+            } else {
+                break;
+            }
+        }
+
+        j = this.column - 1;
+        
+        for (j; j >= 0; j--) {
+            if (board[i][j] === null) {
+                listOfCoordinates.push({ row: i, column: j });
+            } else if (board[i][j].color !== this.color) {
+                listOfCoordinates.push({ row: i, column: j });
+                break;
+            } else {
+                break;
+            }
+        }
+
+        j = this.column;
+        i = this.row + 1
+
+        for (i; i < 8; i++) {
+            if (board[i][j] === null) {
+                listOfCoordinates.push({ row: i, column: j });
+            } else if (board[i][j].color !== this.color) {
+                listOfCoordinates.push({ row: i, column: j });
+                break;
+            } else {
+                break;
+            }
+        }
+        
+        i = this.row - 1
+        
+        for (i; i >= 0; i--) {
+            if (board[i][j] === null) {
+                listOfCoordinates.push({ row: i, column: j });
+            } else if (board[i][j].color !== this.color) {
+                listOfCoordinates.push({ row: i, column: j });
+                break;
+            } else {
+                break;
+            }
+        }
+
+        return listOfCoordinates;
     }
 }
