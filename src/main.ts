f21ea1ba9927c65551412619de9b889277185c5f
walteralleyz <walterdasilvasantos@gmail.com>
2020-10-07 17:27:56 -0300

    seconds clock added, next player movement, chess tiles background changed for tiny ones

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,38 @@ import { calculateBoardSize } from "./boardSizeCalc";
 calculateBoardSize();
 initializePieces(boardMatrix);
 renderBoard(boardMatrix, board);
+setInfo();
+
+var seconds: number = 0;
+var interval = setInterval(setTimer, 1000);
+var color = playerColor;
 
 board.addEventListener('click', ({ target }) => {
   playerMovement(target);
   checkEndGame(playerColor, boardMatrix);
   renderBoard(boardMatrix, board);
+  setInfo();
   console.log(isInCheck(findKingCoord(playerColor), playerColor, boardMatrix));
 });
+
+function setTimer(): any {
+  const timer: HTMLElement = document.querySelector('.info__timer');
+  
+  seconds++;
+  timer.textContent = String(seconds).padStart(2, '0');
+}
+
+function setColor(): void {
+  const color: HTMLElement = document.querySelector('.info__color');
+
+  color.textContent = playerColor.substring(0, 1).toUpperCase() + playerColor.substring(1);
+}
+
+function setInfo(): any {
+  if(color !== playerColor) {
+    color = playerColor;
+    seconds = 0;
+  }
+
+  setColor();
+}
