ede513abd725aca296f9983bd4067b31605d3fe3
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2020-10-10 16:49:53 -0400

    Change the square style when a piece is selected (#18)
    * basic selection highlighting added, some style issues from grid
    
    * added markers for possible piece movements on click of piece
    
    * markers now distinguish between empty spaces and spaces with pieces
    
    * added a few more comments

--- src/clicksEvents.ts
+++ src/clicksEvents.ts
@@ -6,6 +6,17 @@ import { isInCheck } from "./check";
 
 export let playerColor: Color = 'white'; 
 let hasClickedAPiece = false;
+
+// a reference to the currently selected div
+let selectedElement: null | HTMLDivElement = null;
+
+// an array of divs that are a part of all possible moves
+// relative to the selectedElement. I am choosing to store this
+// rather than dynamically create and destroy the array each selection to 
+// save on computations that would otherwise stem from calculating possible moves twice: 
+// once for creation, once for deletion
+let hightlightedElements: HTMLElement[] = [];
+
 let firstPieceDTO: IPieceDTO;
 let secondPieceDTO: IPieceDTO;
 
@@ -18,18 +29,84 @@ export function findPiecePosition(target: HTMLDivElement): IPieceDTO {
   return { coord, pieceInstance };
 }
 
+// this function will grab the div elements of all the
+// possible moves for the selected piece, append a child and toggle specific 
+// classes in order to visually indicate the available moves. 
+function highlightPossibleMoves(target: EventTarget) {
+  if (!selectedElement) return;
+
+  const pieceDTO = findPiecePosition((target as HTMLDivElement));
+
+  const possibleMoves = pieceDTO.pieceInstance.possibleMovementsList(boardMatrix);
+
+  // I needed to grab the parent board div so I would be able to loop through the board
+  const boardElement = (target as HTMLDivElement).parentElement;
+
+  // these are the actual divs in browser
+  const piecesElements = Array.from(boardElement.children, (child) => child as HTMLDivElement);
+
+  // filter the piecesElements array to only the elements that have the same 
+  // row and column attributes of each possible move coordinate
+  const possibleMovesElements = piecesElements.filter(element => {
+    return possibleMoves.some((coord) => {
+      return coord.row === parseInt(element.getAttribute("row"), 10) &&
+      coord.column === parseInt(element.getAttribute("column"), 10)
+    });
+  })
+
+  // append a marker to each possible move, the marker is either
+  // movealble (a blank space) or targetable (a space containing a piece)
+  possibleMovesElements.forEach(element => {
+    const marker = document.createElement('div');
+    // if the position has a piece, marker is targetable
+    marker.className = findPiecePosition(element).pieceInstance ? "targetable" : "moveable";
+    element.appendChild(marker);
+  })
+
+  hightlightedElements = possibleMovesElements;
+}
+
+// this function will get called each time a new piece is selected, 
+// as well as after each move made
+function clearHighlightedMoves() {
+  if (hightlightedElements.length === 0) {
+    return;
+  }
+
+  hightlightedElements.forEach(element => {
+    // there should not be more than firstChild since I manually appended the
+    // additional child (see function highlightPossibleMoves)
+    element.removeChild(element.firstChild);
+  })
+
+  // reset the array
+  hightlightedElements = [];
+}
+
 export function firstClick(target: EventTarget): boolean {
   if (!hasClickedAPiece) {
     const { coord, pieceInstance } = findPiecePosition((target as HTMLDivElement));
     firstPieceDTO = { coord, pieceInstance };
-    // console.log(isInCheck(coord, playerColor));
     if (pieceInstance && pieceInstance.color === playerColor) {
       hasClickedAPiece = true
-      console.log(pieceInstance.color);
+
+      selectedElement = target as HTMLDivElement;
+      highlightPossibleMoves(target);
+      selectedElement.classList.toggle("selected");
+
       return true
     }
   }
-  return false
+  
+  else {
+    // if there is a selectedElement, then the class "selected" was added
+    // and therefore should be removed
+    selectedElement?.classList.remove("selected")
+    clearHighlightedMoves()
+
+    return false
+  }
+
 }
 
 export function secondClick(target: EventTarget) {
